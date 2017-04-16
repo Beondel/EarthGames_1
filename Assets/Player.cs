29d97bc94255d39ecdee951c19e3a9b317344243
Beondel <bengmacmillan@gmail.com>
2017-04-16 15:24:33 -0700

    added player control and sprites for photons

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private float speed;
+
+	void Start () {
+		speed = 5;
+	}
+		
+	void Update () {
+		float axisX = Input.GetAxis("Horizontal");
+		float axisY = Input.GetAxis("Vertical");
+		transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed);
+	}
+}
